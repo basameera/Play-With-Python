5c8391089949de603f9c1d914324063bd3718084
basameera <basameera@pm.me>
2019-06-21 14:25:15 +0200

    run by project name

---
+++ Project Structure/Structure with custom module folder inside/SkunkWork/utils.py
@@ -0,0 +1,18 @@
+"""SkunkWork Utils"""
+import datetime
+
+
+def clog(*args):
+    msg = '>>> '+str(datetime.datetime.now()).split('.')[0] + ' :'
+    for s in args:
+        msg = msg + ' ' + str(s)
+    print(msg)
+
+
+def main():
+    clog('Hello World!')
+
+
+    # run
+if __name__ == '__main__':
+    main()
