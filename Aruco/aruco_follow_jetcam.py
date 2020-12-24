99a0aea474789ede87a628675a77bb04fc4b256b
basameera <basameera@protonmail.com>
2020-12-24 17:15:17 +0100

    jetcam version

--- Aruco/aruco_follow_jetcam.py
+++ Aruco/aruco_follow_jetcam.py
@@ -1,14 +1,11 @@
+import setup_path
 import cv2
+from jetcam.csi_camera import CSICamera
 import utils as ut
-
-
-def image_resize(image, h, w):
-    return cv2.resize(image, (h, w), interpolation=cv2.INTER_AREA)
 
 
 if __name__ == "__main__":
     arucoDict, arucoParams = ut.aruco_init()
-    cap = cv2.VideoCapture(0)
 
     image_height = 720
     image_width = 1280
@@ -19,14 +16,11 @@ if __name__ == "__main__":
 
     centers = None
 
-    # Check if the webcam is opened correctly
-    if not cap.isOpened():
-        raise IOError("Cannot open webcam")
+    camera = CSICamera(width=image_width, height=image_height, capture_width=1280,
+                       capture_height=720, capture_fps=30, flip_mode=2)
 
     while True:
-        ret, image = cap.read()
-        # remove for jetcam - its native 720
-        image = image_resize(image, 1280, 720)
+        image = camera.read()
 
         (corners, ids, rejected) = cv2.aruco.detectMarkers(
             image, arucoDict, parameters=arucoParams)
@@ -38,19 +32,19 @@ if __name__ == "__main__":
             # only the first Aruco detected will be followed
             # NOTE: need a method to handle multiple Aruco
             center_x, center_y = centers[0]
-
-            (quad_coord_x, quad_coord_y) = ut.cam_coord_to_quadrant_coord(
-                (center_x, center_y), (image_center_x, image_center_y))
+            print(center_x, center_y)
+            # (quad_coord_x, quad_coord_y) = ut.cam_coord_to_quadrant_coord(
+                # (center_x, center_y), (image_center_x, image_center_y))
 
             # print(center_x, center_y, ' | ', quad_coord_x, quad_coord_y)
-            turn_direction = ''
-            if quad_coord_x > safe_zone_sz_x:
-                turn_direction = 'right'
-            elif quad_coord_x < (-safe_zone_sz_x):
-                turn_direction = 'left'
-            else:
-                turn_direction = 'STOP'
-            print(center_x, ' | ', quad_coord_x, ' | ', turn_direction)
+            # turn_direction = ''
+            # if quad_coord_x > safe_zone_sz_x:
+            #     turn_direction = 'right'
+            # elif quad_coord_x < (-safe_zone_sz_x):
+            #     turn_direction = 'left'
+            # else:
+            #     turn_direction = 'STOP'
+            # print(center_x, ' | ', quad_coord_x, ' | ', turn_direction)
 
         cv2.imshow('Input', image)
 
@@ -58,5 +52,4 @@ if __name__ == "__main__":
         if c == 27:
             break
 
-    cap.release()
     cv2.destroyAllWindows()
