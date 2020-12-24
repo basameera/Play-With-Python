aecc6672cad804b173402dabce211c52a12e0bf3
basameera <basameera@protonmail.com>
2020-12-24 14:46:07 +0100

    aruco jetcam

---
+++ Aruco/aruco_jetcam.py
@@ -0,0 +1,31 @@
+import setup_path
+import cv2
+from jetcam.csi_camera import CSICamera
+import utils as ut
+
+
+if __name__ == "__main__":
+    arucoDict, arucoParams = ut.aruco_init()
+
+    width=1280
+    height=720
+    camera = CSICamera(width=width, height=height, capture_width=1280,
+                    capture_height=720, capture_fps=30, flip_mode=2)
+
+    while True:
+        image = camera.read()
+
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(
+            image, arucoDict, parameters=arucoParams)
+
+        if ids is not None:
+            image = ut.drawAruco(image, ids, corners)
+
+        cv2.imshow('Input', image)
+
+        c = cv2.waitKey(1)
+        if c == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ Aruco/setup_path.py
@@ -0,0 +1,4 @@
+# add custom library paths to list
+import os
+import sys
+sys.path.insert(0, '/home/ackman/Github/jetcam')
